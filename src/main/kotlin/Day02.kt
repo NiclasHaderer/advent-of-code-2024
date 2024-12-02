9621c0aec774faa1e244202452f5b5247eb12b7a
Niclas Haderer <niclas.haderer.dev@gmail.com>
2024-12-02 23:55:58 +0100

    Cleanup

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -4,11 +4,16 @@ object Day02 {
 
     private fun isSave(ints: List<Int>): Boolean {
         if (ints.size < 2) return true
-        val diffs = ints.zipWithNext { a, b -> b - a }
+        val diffs = ints.zipWithNext { a, b -> a - b }
         val isIncreasing = diffs.all { it in 1..3 }
         val isDecreasing = diffs.all { it in -3..-1 }
 
         return isIncreasing || isDecreasing
+    }
+
+    private fun withRemovedItemsIsSave(ints: List<Int>): Boolean = ints.indices.any { i ->
+        // Is it quick? No. Is it late? Yes!
+        ints.toMutableList().also { it.removeAt(i) }.let(::isSave)
     }
 
     fun part1(input: List<List<Int>>): Int = input.count(::isSave)
@@ -17,8 +22,4 @@ object Day02 {
         isSave(ints) || withRemovedItemsIsSave(ints)
     }
 
-    fun withRemovedItemsIsSave(ints: List<Int>): Boolean = ints.indices.any { i ->
-        // Is it quick? No. Is it late? Yes!
-        ints.toMutableList().also { it.removeAt(i) }.let(::isSave)
-    }
 }
